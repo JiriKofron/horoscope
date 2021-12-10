75df2a7bfeb27a43325c9f547217ee979c7bee52
Jiří Kofroň <kofron.jiri@gmail.com>
2021-12-10 23:15:10 +0100

    final working version

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const wheel = document.querySelector("img");
-const zodiacSign = document.querySelector(('.zodiac-sign'));
-const date = document.querySelector('.date');
-const btn = document.querySelector('.btn');
+const zodiacSign = document.querySelector('.zodiac-sign__header__h2');
+const zodiacSignText = document.querySelector('.zodiac-sign__text');
+const zodiacSignContainer = document.querySelector('.zodiac-sign__info');
+const date = document.querySelector('.zodiac-sign__date');
+const btn = document.querySelector('.zodiac-sign__btn');
 let inputIndex = null;
 let zodiacArray = [];
 const getData = function (){
@@ -18,15 +20,22 @@ btn.addEventListener('click', function () {
     const inputDay = ("0" + new Date(date.value).getDate()).slice(-2)
     const inputMonth = new Date(date.value).getMonth();
     inputIndex = `${inputMonth + 1}${inputDay}`;
+    console.log(inputIndex)
 
     for (let sign of zodiacArray[0] ){
-        if (inputIndex > sign.startIndex && inputIndex < sign.endIndex){
-            console.log(sign.nazev)
+        if (sign.id === 9 && (inputIndex >= 1221 || inputIndex <= 120)) {
             zodiacSign.innerHTML = sign.nazev
-        } else if(inputIndex > 1121 || inputIndex < 19) {
-            zodiacSign.innerHTML = " Kozoroh"
+            zodiacSignText.innerHTML = sign.text[0]
+        }
+        else if (inputIndex >= sign.startIndex && inputIndex <= sign.endIndex){
+            zodiacSign.innerHTML = sign.nazev
+            zodiacSignText.innerHTML = sign.text[0]
         }
     }
+
+    zodiacSignText.style.opacity = 1;
+    zodiacSign.style.opacity = 1;
+    zodiacSignContainer.style.opacity = 1;
 
     console.log(zodiacSign.innerHTML);
     switch (zodiacSign.innerHTML) {
@@ -34,10 +43,37 @@ btn.addEventListener('click', function () {
             wheel.style.transform = "rotate(0deg)"
             break;
         case "Býk":
-            wheel.style.transform = "rotate(-25deg)"
+            wheel.style.transform = "rotate(-30deg)"
             break;
         case "Blíženec":
-            wheel.style.transform = "rotate(-55deg)"
+            wheel.style.transform = "rotate(-60deg)"
+            break;
+        case "Rak":
+            wheel.style.transform = "rotate(-90deg)"
+            break;
+        case "Lev":
+            wheel.style.transform = "rotate(-120deg)"
+            break;
+        case "Panna":
+            wheel.style.transform = "rotate(-150deg)"
+            break;
+        case "Váhy":
+            wheel.style.transform = "rotate(-180deg)"
+            break;
+        case "Štír":
+            wheel.style.transform = "rotate(-210deg)"
+            break;
+        case "Střelec":
+            wheel.style.transform = "rotate(-240deg)"
+            break;
+        case "Kozoroh":
+            wheel.style.transform = "rotate(-270deg)"
+            break;
+        case "Vodnář":
+            wheel.style.transform = "rotate(-300deg)"
+            break;
+        case "Ryby":
+            wheel.style.transform = "rotate(-330deg)"
             break;
         default:
             wheel.style.transform = "rotate(0deg)"
